9498c9497ac5a84438c06d32627f14e852585d10
bayogluteoman <bayogluteoman@gmail.com>
2022-01-31 01:19:32 +0300

    fix toggle button

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  function handleClick() {
+    setIsOn((isOn) => !isOn);
+  }
+
+  const color = isOn ? "red" : "green";
+  const text= isOn ? <h1 style={{ background: "green" }}>Hello, you are in</h1> : <h1 style={{ background: "red" }}>Goodbye</h1>
+
+  return (
+    <div>
+    <button style={{ background: color }} onClick={handleClick}>
+      {isOn ? "OFF" : "ON"}
+    </button>
+      {text}
+    </div>
+  );
+
 }
 
 export default Toggle;
